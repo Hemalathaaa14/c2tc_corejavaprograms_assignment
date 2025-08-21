871c1a625ca901002476dc8aadadc928c82ec87e
hemalatha M <hemalathamuniyan@gmail.com>
2025-08-21 09:32:06 +0530

    Assignment 4 updated

---
+++ src/assignment4/Bank.java
@@ -0,0 +1,13 @@
+package assignment4;
+
+public class Bank {
+    private static int totalAccounts = 0;
+
+    public static void incrementTotalAccounts() {
+        totalAccounts++;
+    }
+
+    public static int getTotlaAccounts() {
+        return totalAccounts;
+    }
+}
